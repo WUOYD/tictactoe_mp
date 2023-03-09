dee5b15b4735e9cb74306b53deb64bfd773de9d3
Julian Birrer <julian.birrer@bluewin.ch>
2023-03-09 21:43:16 +0100

    Sockets integration with bugs

--- public/client.js
+++ public/client.js
@@ -1,11 +1,10 @@
-//Sockets
 var socket = io()
-
-socket.on("click", data => {
-  let formName = "form" + data
-  let form = document.getElementById(formName);
-  drawForm(data, form);
-});
+let myTurn = false;
+let myTurnIndicator = document.querySelector(".myTurn");
+let waitIndicator = document.querySelector(".wait");
+let gameStart = document.querySelector(".start");
+let gameState = document.querySelector(".status");
+let gameRestart = document.querySelector(".restart");
 
 let grid = [0,0,0,0,0,0,0,0,0]
 let player = 0
@@ -33,58 +32,79 @@ var form8 = document.getElementById('form8');
 
 tile0.addEventListener('click', function (event) {
   drawForm(0, form0);
-  socket.emit("click", 0);
 });
 tile1.addEventListener('click', function (event) {
   drawForm(1, form1);
-  socket.emit("click", 1);
 });
 tile2.addEventListener('click', function (event) {
   drawForm(2, form2);
-  socket.emit("click", 2);
 });
 tile3.addEventListener('click', function (event) {
   drawForm(3, form3);
-  socket.emit("click", 3);
 });
 tile4.addEventListener('click', function (event) {
   drawForm(4, form4);
-  socket.emit("click", 4);
 });
 tile5.addEventListener('click', function (event) {
   drawForm(5, form5);
-  socket.emit("click", 5);
 });
 tile6.addEventListener('click', function (event) {
   drawForm(6, form6);
-  socket.emit("click", 6);
 });
 tile7.addEventListener('click', function (event) {
   drawForm(7, form7);
-  socket.emit("click", 7);
 });
 tile8.addEventListener('click', function (event) {
   drawForm(8, form8);
-  socket.emit("click", 8);
 });
 
 function drawForm(tile, elem){
-  if(grid[tile] == 1 || grid[tile] == 2){
+  if (myTurn) {
+    if(grid[tile] == 1 || grid[tile] == 2){
+      console.log("Already occupied");
+    }
+    else if(!gameRestart.classList.contains("hidden")){
+    }
+    else{
+      if(player % 2 == 0){
+        elem.classList.add('circle')
+        grid[tile] = 1
+      }
+      else if(player % 2 == 1){
+        elem.classList.add('cross')
+        grid[tile] = 2
+      }
+      else{console.log("Error")}
+      player++
+      checkWin()
+      socket.emit("click", tile);
+      socket.emit('turn', true)
+
+      myTurn = false;
+      myTurnIndicator.classList.toggle("hidden");
+      waitIndicator.classList.toggle("hidden");
+    }
   }
-  else{
-    if(player % 2 == 0){
-      elem.classList.add('circle')
-      grid[tile] = 1
-    }
-    else if(player % 2 == 1){
-      elem.classList.add('cross')
-      grid[tile] = 2
-    }
-    else{console.log("Error")}
-    player++
-    checkWin()
+  else {
+    console.log("Not your turn");
   }
 }
+
+function drawFormEnemy(tile, elem){
+      if(player % 2 == 0){
+        elem.classList.add('circle')
+        grid[tile] = 1
+      }
+      else if(player % 2 == 1){
+        elem.classList.add('cross')
+        grid[tile] = 2
+      }
+      else{console.log("Error")}
+      player++
+      checkWin()
+}
+
+
 
 function checkWin(){
   //Circle
@@ -143,11 +163,11 @@ function checkWin(){
 function win(winner){
   if(winner == "circle"){
     document.querySelector('#winner').innerHTML = "Circle Wins"
-    document.getElementById("restart").style.visibility = "visible"
+    gameRestart.classList.toggle("hidden");
   }
   else if (winner == "cross"){
     document.querySelector('#winner').innerHTML = "Cross Wins"
-    document.getElementById("restart").style.visibility = "visible"
+    gameRestart.classList.toggle("hidden");
   }
   else{}
 }
@@ -155,7 +175,10 @@ function win(winner){
 document.getElementById("restartButton").addEventListener("click", ()=>{
   grid = [0,0,0,0,0,0,0,0,0]
   player = 0
-  document.getElementById("restart").style.visibility = "hidden"
+  gameRestart.classList.toggle("hidden");
+  gameStart.classList.toggle("hidden");
+  gameState.classList.toggle("hidden");
+  socket.emit("restart", true);
 
   const elements = document.getElementsByClassName('forms');
 
@@ -163,6 +186,28 @@ document.getElementById("restartButton").addEventListener("click", ()=>{
     el.classList.remove('circle', 'cross');
   });
 })
+
+//Sockets
+socket.on("click", data => {
+  let formName = "form" + data
+  let form = document.getElementById(formName);
+  drawFormEnemy(data, form);
+});
+
+socket.on("turn", myTurnFromServer => {
+  myTurn = myTurnFromServer;
+  myTurnIndicator.classList.toggle("hidden");
+  waitIndicator.classList.toggle("hidden");
+});
+
+socket.on("start", startgame => {
+  if(startgame == true){
+    gameStart.classList.toggle("hidden");
+    gameState.classList.toggle("hidden");
+  }
+  else{}
+});
+
 
 
 
--- server.js
+++ server.js
@@ -5,13 +5,48 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let sockets = [];
+let restart = 0;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use(express.static("public"));
 
+
+
 io.on('connection', (socket) => {
+  sockets.push(socket);
+
+  if (sockets.length == 1) {
+    socket.emit("turn", true);
+  }
+  else if (sockets.length == 2) {
+    socket.emit("start", true);
+    socket.broadcast.emit("start", true);
+  }
+
   socket.on('click', (tile) => {
     socket.broadcast.emit('click', tile)
+    socket.broadcast.emit("turn", true);
   })
+
+  socket.on('restart', status => {
+    restart++
+    if(restart==2){
+      socket.emit("start", true);
+      socket.broadcast.emit("start", true);
+      restart=0;
+    }
+  })
+
+  socket.on('disconnect', function() {
+    console.log('Got disconnect!');
+    let i = sockets.indexOf(socket);
+    sockets.splice(i, 1);
+  });
+
 })
 
 server.listen(3000, () => {
